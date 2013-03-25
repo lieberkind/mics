38064235f93b2f18493ef125549d47fcf978dcaf
Asger Schlichtkrull <asgsch@gmail.com>
2013-03-25 15:59:17 +0100

    Added ScriptSharp namespace

--- MiCSTests/UnitTest1.cs
+++ MiCSTests/UnitTest1.cs
@@ -1,14 +1,26 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using ScriptSharp.Generator;
+using System.IO;
+using ScriptSharp;
+using ScriptSharp.ScriptModel;
 namespace MiCSTests
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void SimpleScriptSharpNamespaceTest()
         {
+            var stringWriter = new StringWriter();
+            var writer = new ScriptTextWriter(stringWriter);
+            var options = new CompilerOptions();
+            var generator = new ScriptGenerator(writer, options, null);
+
+            var l = new LiteralExpression(null, true);
+            ExpressionGenerator.GenerateExpression(generator, null, l);
+
+            Assert.IsTrue(Boolean.Parse(stringWriter.ToString()));
         }
     }
 }
