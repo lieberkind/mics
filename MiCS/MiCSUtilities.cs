f3315a655edea7ff874f49892696c00198bbaa1a
Tomas Lieberkind <t.lieberkind@gmail.com>
2013-03-28 01:25:43 +0100

    Added MiCSUtilities class for various helper functions. Currently contains methods to get a SyntaxTree for a specific file in the current solution. Tests have been added in MiCSUtilitiesTests

---
+++ MiCS/MiCSUtilities.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using EnvDTE;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Roslyn.Services;
+using Roslyn.Compilers.CSharp;
+using System.IO;
+
+namespace MiCS
+{
+    public static class MiCSUtilities
+    {
+
+        private static string GetCurrentSolutionDirectory()
+        {
+            // Solution found here: http://stackoverflow.com/questions/2054182/programmatically-getting-the-current-visual-studio-ide-solution-directory-from-a
+            EnvDTE.DTE dte = (EnvDTE.DTE)System.Runtime.InteropServices.Marshal.GetActiveObject("VisualStudio.DTE");
+            return dte.Solution.FullName;
+        }
+
+        public static SyntaxTree GetSyntaxTree(string fileName)
+        {
+            string currentSolutionDir = MiCSUtilities.GetCurrentSolutionDirectory();
+
+            IWorkspace ws = Workspace.LoadSolution(currentSolutionDir);
+            ISolution s = ws.CurrentSolution;
+
+            IEnumerable<IProject> projects = s.Projects;
+
+            foreach (var project in projects)
+            {
+                var documents = project.Documents;
+
+                foreach (var document in documents)
+                {
+                    if (document.Name == fileName)
+                    {
+                        SyntaxTree syntaxTree = (SyntaxTree)document.GetSyntaxTree();
+                        return syntaxTree;
+                    }
+                }
+            }
+            throw new FileNotFoundException("File " + fileName + " was not found in solution");
+        }
+
+        public static SyntaxTree GetSyntaxTree(string solutionPath, string fileName)
+        {
+            string currentSolutionDir = solutionPath;
+
+            IWorkspace ws = Workspace.LoadSolution(currentSolutionDir);
+            ISolution s = ws.CurrentSolution;
+
+            IEnumerable<IProject> projects = s.Projects;
+
+            foreach (var project in projects)
+            {
+                var documents = project.Documents;
+
+                foreach (var document in documents)
+                {
+                    if (document.Name == fileName)
+                    {
+                        SyntaxTree syntaxTree = (SyntaxTree)document.GetSyntaxTree();
+                        return syntaxTree;
+                    }
+                }
+            }
+            throw new FileNotFoundException("File " + fileName + " was not found in solution");
+        }
+    }
+}
---
+++ MiCSTests/MiCSUtilitiesTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MiCS;
+using System.IO;
+using Roslyn.Compilers.CSharp;
+
+namespace MiCSTests
+{
+    [TestClass]
+    public class MiCSUtilitiesTests
+    {
+        [TestMethod]
+        public void SyntaxTreeIsReturnedWhenFileExistsInSolution()
+        {
+            var syntaxTree = MiCSUtilities.GetSyntaxTree("MiCSUtilitiesTests.cs");
+            Assert.IsInstanceOfType(syntaxTree, typeof(SyntaxTree));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void ExceptionIsThrownWhenFileDoesNotExistInSolution() 
+        {
+            var syntaxTree = MiCSUtilities.GetSyntaxTree("AFileThatNeverExists.random");
+        }
+    }
+}
