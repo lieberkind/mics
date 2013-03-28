8518d25987836f38ac0e5d60e61031ea6b80248e
Asger Schlichtkrull <asgsch@gmail.com>
2013-03-28 11:16:21 +0100

    Added two simple class member tests.

--- MiCSTests/MapTests.cs
+++ MiCSTests/MapTests.cs
@@ -27,9 +27,6 @@ namespace MiCSTests
             Assert.AreEqual(RosNamespace.Name.ToString(), SSNamespace.Name);
         }
 
-
-
-
         [TestMethod]
         public void NamespaceMemberTest()
         {
@@ -67,9 +64,37 @@ namespace MiCSTests
 
             var RosMethod = (MethodDeclarationSyntax)RosMember.Members.First();
             var SSMethod = (ScriptSharp.ScriptModel.MethodSymbol)SSMember.Members.First();
+
             Assert.AreEqual(RosMethod.Identifier.ValueText, SSMethod.Name);
             Assert.AreEqual(RosMethod.Body.Statements.Count, SSMethod.Implementation.Statements.Count);
         }
 
+        [TestMethod]
+        public void ClassMemberTest()
+        {
+            var source = @"
+            namespace TestNamespace { 
+                class TestClass { 
+                    [MixedSide]
+                    void f() { int i; }
+                } 
+            }";
+            var RosNamespace = (NamespaceDeclarationSyntax)Parse.Namespaces(source).First();
+            var SSNamespace = RosNamespace.Map();
+
+            var RosMember = (ClassDeclarationSyntax)RosNamespace.Members.First();
+            var SSMember = (ClassSymbol)SSNamespace.Types.First();
+
+            var RosMethod = (MethodDeclarationSyntax)RosMember.Members.First();
+            var SSMethod = (ScriptSharp.ScriptModel.MethodSymbol)SSMember.Members.First();
+
+            var RosStmt = (LocalDeclarationStatementSyntax)RosMethod.Body.Statements.First();
+            var SSStmt = (VariableDeclarationStatement)SSMethod.Implementation.Statements.First();
+
+            var RosVar = RosStmt.Declaration.Variables.First();
+            var SSVar = SSStmt.Variables.First();
+
+            Assert.AreEqual(RosVar.Identifier.ValueText, SSVar.Name);
+        }
     }
 }
