e762f5d24cfc4b5de4ffd9e51a2c1eb0ded940ec
Tomas Lieberkind <t.lieberkind@gmail.com>
2013-03-28 13:15:05 +0100

    Added collector methods to get all classes with methods marked with the MixedSide attribute

---
+++ MiCS/MiCSAttributes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiCS
+{
+    [System.AttributeUsage(System.AttributeTargets.Method)]
+    public class MixedSide : System.Attribute
+    {
+    }
+
+    [System.AttributeUsage(System.AttributeTargets.Method)]
+    public class ClientSide : System.Attribute
+    {
+    }
+}
