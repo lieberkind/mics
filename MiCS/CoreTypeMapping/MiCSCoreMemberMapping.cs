bb52d1445b2266a12e75289d7430f3f1d6aa4e06
Asger Schlichtkrull <asgsch@gmail.com>
2013-04-26 21:43:30 +0200

    Added comments to MiCS core type mapping classes.

--- MiCS/CoreTypeMapping/MiCSCoreMemberMapping.cs
+++ MiCS/CoreTypeMapping/MiCSCoreMemberMapping.cs
@@ -7,6 +7,13 @@ using System.Threading.Tasks;
 
 namespace MiCS
 {
+    /// <summary>
+    /// Member mapping specification from C# core type member to ScriptSharp core type member.
+    /// Name property value is mapped to the NameScript property value. Argument types and 
+    /// return type should be the types from the ScriptSharp core types (which then is required
+    /// to be matched on the equivalent C# core type member by the TypeManager for the core 
+    /// type usage to be valid).
+    /// </summary>
     class MiCSCoreMemberMapping
     {
         public string Name { get; set; }
